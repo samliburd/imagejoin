90280aacf5a2ca849609dda3a0226e72cf4adc25
samliburd <liburdsam@gmail.com>
2025-04-06 01:36:54 +0100

    live

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,38 +2,57 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].[contenthash].js',
+    clean: true,
+  },
   module: {
     rules: [
       {
         test: /\.scss$/i,
         use: [
-          'style-loader', // Inject styles into DOM
-          'css-loader', // Turns CSS into CommonJS
+          MiniCssExtractPlugin.loader,
+          'css-loader',
           {
-            loader: 'sass-loader', // Compiles Sass to CSS
+            loader: 'sass-loader',
             options: {
               sassOptions: {
-                includePaths: ['node_modules'], // Ensures node_modules are available for imports
+                includePaths: ['node_modules'],
               },
             },
           },
         ],
       },
       {
-        test: /\.css$/i, // This rule is for CSS files
+        test: /\.css$/i,
         use: [
-          'style-loader', // Inject CSS into DOM
-          'css-loader', // Turns CSS into CommonJS
+          MiniCssExtractPlugin.loader,
+          'css-loader',
         ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name].[hash][ext]',
+        },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
     }),
     new CopyPlugin({
       patterns: [
